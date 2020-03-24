953b922c7ff9b6ff836937d506f08bf85318b136
Aung Khant Thaw <aungkthaw@gmail.com>
2020-03-25 02:05:42 +0630

    Route_Test_OK_24_3_20

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,24 +1,35 @@
 var axios = require('axios');
 var express = require('express');
 var router = express.Router();
-var routes = ['/contact',
-             ];
+var routes = ['/contact/:state/:hospital',
+              '/contact/:state/:hospital/:id'];
 
-var routeFunctions = [AllHospital
-                     ];
+var routeFunctions = [AllHospital,
+                      OneHospital];
 
 router.use(function(req,res,next)
 {
     console.log('%s %s %s', req.method, req.url, req.path);
+    next();
 });
 
 router.get(routes[0],routeFunctions[0]);
+router.get(routes[1],routeFunctions[1]);
 
 function AllHospital(req,res,next)
 {
-    console.log("work");
-    
+    console.log("AllHospital");
+    console.table(req.params)
     console.log(routes[0]);
+    res.send(req.params);
+}
+
+function OneHospital(req,res,next)
+{
+    console.log("OneHospital");
+    console.table(req.params)
+    console.log(routes[0]);
+    res.send(req.params);
 }
 
 module.exports = router;
