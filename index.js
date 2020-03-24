79d88e97aa51f63e2353d480d287978b845ef4a3
Aung Khant Thaw <aungkthaw@gmail.com>
2020-03-25 02:05:42 +0630

    Route_Test_OK_24_3_20

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/api/',apiRouter);
+app.use('/api',apiRouter);
 
 app.listen(port);
 console.log('starting');
