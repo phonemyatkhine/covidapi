43ea97a5b06953f3cb2143ba66b29338a57d202e
Aung Khant Thaw <aungkthaw@gmail.com>
2020-03-27 02:28:45 +0630

    CodeOnServer

---
+++ app.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+      name        : "CovidApi",
+      script      : "app.js",
+      watch       : true,
+      merge_logs  : true,
+      cwd         : "/var/www/covidapi/",
+     }]
+  }
