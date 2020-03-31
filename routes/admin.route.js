f4bd5d3539d6909c8edc109a91be55c88263113d
Hein Thanth <official.heinthanth@gmail.com>
2020-03-31 14:10:13 +0630

    My bad - Fix admin login, register

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -9,93 +9,109 @@ require("dotenv").config();
 module.exports = router;
 const expiration = 400000;
 
-router.get("/login", function(req, res) {
-	res.render("login");
-});
-
-// wildcard regex route for redirect login
-/* 
- *	admin/login/r/(foo)
- *	will redirect to foo
-*/
-router.get(/\/login\/r(.*)/, function(req, res) {
-	res.render("login", { redirect: req.params[0] });
+router.get(/\/login(\/r=(.*))?/, (req, res) => {
+	// above regex resolve /login/r=uri-to-redirect
+	const render_data = { redirect_to: undefined };
+	const redirect_url = req.params[1];
+	if (redirect_url) {
+		render_data = { redirect_to: redirect_url };
+	}
+	// redirect_url will embed to login page
+	res.render("login", render_data);
 });
 
 router.post("/login", async (req, res) => {
-	console.log("Log In Post");
-	console.log(req.body);
-	let redirect = req.body.redirect || null;
+	let redirect_url = req.body.redirect_to;
+	const render_data = { redirect_to: undefined };
+	if (redirect_url) {
+		render_data = { redirect_to: redirect_url };
+	}
 	let email = req.body.email;
 	let password = req.body.password;
-	let hashedPassword = crypto
-		.createHash("md5")
-		.update(password)
-		.digest("hex");
+
 	var token = null;
 
 	if (req.cookies.token != null) {
 		token = req.cookies.token;
 	}
-	if (token != null) {
-		jwt.verify(token, process.env.KEY, (err, ele) => {
+
+	if (token) {
+		jwt.verify(token, process.env.KEY, (err, _nil) => {
 			if (err) {
-				res.render("login", { redirect: redirect });
+				res.render("login", render_data);
 			}
 		});
-		if (redirect) {
-			res.redirect(redirect);
+		if (redirect_url) {
+			res.redirect(redirect_url);
 		} else {
 			res.redirect("/admin");
 		}
 	} else {
 		try {
-			var admin = await Admin.find({
-				email: email,
-				password: hashedPassword
+			var admin = await Admin.findOne({
+				email: email
 			});
-			if (admin.length != 0) {
-				token = jwt.sign(
-					{
-						email,
-						password
-					},
-					process.env.KEY,
-					{
-						expiresIn: "1d"
-					}
-				);
-				res.status(200)
-					.cookie("token", token, {
-						expires: new Date(Date.now() + expiration),
-						secure: false,
-						httpOnly: true
-					})
-					.then(() => {
-						res.redirect(redirect);
-					});
+			if (admin) {
+				salt = admin.salt;
+				let input_pass = `${password}${salt}`;
+				let hashedPassword = crypto
+					.createHash("sha1")
+					.update(input_pass)
+					.digest("hex");
+				if (hashedPassword == admin.password) {
+					token = jwt.sign(
+						{
+							email,
+							password
+						},
+						process.env.KEY,
+						{
+							expiresIn: "1d"
+						}
+					);
+					res.status(200)
+						.cookie("token", token, {
+							expires: new Date(Date.now() + expiration),
+							secure: false,
+							httpOnly: true
+						})
+						.then(() => {
+							res.redirect(redirect);
+						});
+				} else {
+					res.render("login", render_data);
+				}
 			} else {
-				res.render("login", { redirect: redirect });
+				res.render("login", render_data);
 			}
 		} catch (err) {
-			res.render("login", { redirect: redirect });
+			res.render("login", render_data);
 		}
 	}
 });
 
 router.post("/register", async (req, res) => {
+	// use password salt for uncrackable hash
 	let email = req.body.email;
 	let password = req.body.password;
 	let key = req.body.key;
+	let salt =
+		"_" +
+		Math.random()
+			.toString(36)
+			.substr(2, 9);
+
+	let SaltedPassword = `${password}${salt}`;
 	let hashedPassword = crypto
-		.createHash("md5")
-		.update(password)
+		.createHash("sha1")
+		.update(SaltedPassword)
 		.digest("hex");
-	console.log(req.body);
+
 	if (key == process.env.KEY) {
 		try {
-			var admin = new Admin({
+			let admin = new Admin({
 				email: email,
+				salt: salt,
 				password: hashedPassword
 			});
 			try {
@@ -137,23 +153,6 @@ router.post("/logout", async (req, res) => {
 			.then(() => {
 				res.redirect("/admin/login");
 			});
-	}
-});
-
-router.get("/create-contact", (req, res) => {
-	var token = null;
-	if (req.cookies.token != null) {
-		token = req.cookies.token;
-	}
-	if (token != null) {
-		jwt.verify(token, process.env.KEY, (err, ele) => {
-			if (err) {
-				res.redirect(`/admin/login/r/admin/create-contact`);
-			}
-		});
-		res.render("forminput");
-	} else {
-		res.redirect(`/admin/login/r/admin/create-contact`);
 	}
 });
 
@@ -199,3 +198,19 @@ async function getAdmin(req, res, next) {
 
 	next();
 }
+router.get("/create-contact", (req, res) => {
+	var token = null;
+	if (req.cookies.token != null) {
+		token = req.cookies.token;
+	}
+	if (token != null) {
+		jwt.verify(token, process.env.KEY, (err, ele) => {
+			if (err) {
+				res.redirect(`/admin/login/r/admin/create-contact`);
+			}
+		});
+		res.render("forminput");
+	} else {
+		res.redirect(`/admin/login/r/admin/create-contact`);
+	}
+});
