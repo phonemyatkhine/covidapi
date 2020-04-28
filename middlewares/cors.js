11f40e530e56114cbbd7548c30c2913b1c989866
Hein Thanth <official.heinthanth@gmail.com>
2020-04-28 22:27:18 +0630

    Fix CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,9 @@
 const fn = (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
 };
 
